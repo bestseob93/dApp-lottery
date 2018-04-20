9ad7d9bbe7ab417539694f39d8a4e0faef8f4575
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-20 20:42:22 +0900

    Multiple accounts test added

--- test/Lotto.test.js
+++ test/Lotto.test.js
@@ -35,4 +35,30 @@ describe('Lotto Contract', () => {
     assert.equal(accounts[0], players[0]); // 올바른 주소가 저장됐는지 확인하는 테스트
     assert.equal(1, players.length); // 1개의 레코드만 있는지 확인하는 테스트
   });
+
+  it('allows multiple accounts to enter', async () => {
+    await lotto.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lotto.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lotto.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lotto.methods.getPlayer().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]); // 올바른 주소가 저장됐는지 확인하는 테스트
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length); // 3개의 레코드가 있는지 확인하는 테스트
+  });
 });
