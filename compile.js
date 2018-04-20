239d13bad81cb3dbb016fa50f9bbe581f2bdda2b
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-20 20:20:48 +0900

    Lotto test code updated

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Lotto.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lottoPath = path.resolve(__dirname, 'contracts', 'Lotto.sol');
+const source = fs.readFileSync(lottoPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':Lotto']; // 2번째 인자는 컨트랙트의 갯수(현재는 1)
