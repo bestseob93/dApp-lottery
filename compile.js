a926f7cd1cdbcb41835670ebc6116d8809bb1c13
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-20 20:12:16 +0900

    Lotto Contract

---
+++ compile.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Lotto.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Lotto']; // 2번째 인자는 컨트랙트의 갯수(현재는 1)
